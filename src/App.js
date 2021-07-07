3ef5c59c4faa12b9edf3e9f90a82259960360c67
himadri2110 <himadrishah2000@gmail.com>
2021-07-07 14:50:50 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import "./styles.css";
+import { useState } from "react";
+
+var foodDictionary = {
+  Indian: [
+    ["Pani Puri", "5/5"],
+    ["Pav Bhaji", "4/5"],
+    ["Dosa", "3.5/5"]
+  ],
+  Chinese: [
+    ["Noodles", "4.5/5"],
+    ["Manchurian", "4/5"],
+    ["Spring Rolls", "3/5"]
+  ],
+  Italian: [
+    ["Pasta", "5/5"],
+    ["Pizza", "4.5/5"],
+    ["Lasagna", "4/5"]
+  ]
+};
+
+var foodList = Object.keys(foodDictionary);
+
+export default function App() {
+  var [output, setOutput] = useState("");
+
+  function foodClickHandler(food) {
+    var subList = foodDictionary[food];
+    setOutput(subList);
+  }
+
+  return (
+    <div className="App">
+      <main>
+        <h1>
+          <span role="img" aria-label="noodles">
+            🍜
+          </span>{" "}
+          Food Recommendation App
+        </h1>
+        <p>
+          See a list of my favourite food items. Click a cuisine to get started.
+        </p>
+
+        <div className="genreList">
+          {foodList.map((food) => {
+            return <span onClick={() => foodClickHandler(food)}>{food}</span>;
+          })}
+        </div>
+
+        <hr />
+
+        <div className="showFood">
+          <ul>
+            {Array.from(output).map((item) => {
+              return (
+                <li>
+                  {item[0]}
+                  <br />
+                  <span className="rating">{item[1]}</span>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </main>
+    </div>
+  );
+}
